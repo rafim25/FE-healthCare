09755b3d0859ccc721b0417e0b00c0d4e79b5048
mohammad rafee <mohammad.rafee@piramal.com>
2025-01-10 16:05:43 +0530

    adding hooks for api call

---
+++ helthcare-fe-hcltech/src/app/Hooks/useFetchData.js
@@ -0,0 +1,29 @@
+// pages/Patients/index.tsx
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+// Custom hook for fetching data
+const useFetchData = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetchData;
