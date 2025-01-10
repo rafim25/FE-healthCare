366bd561309616520b045356e1321a10bee156e3
mohammad rafee <mohammad.rafee@piramal.com>
2025-01-10 16:31:24 +0530

    addded haeder

---
+++ helthcare-fe-hcltech/src/app/Components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+      <header className="flex items-center justify-between p-4 bg-blue-600 text-white w-full">
+          <img src="/images/logo.svg" alt="Company Logo" className="h-10" /> {/* Dummy logo */}
+          <h1 className="text-xl font-bold">Bayer Health Management</h1> {/* Company name */}
+      </header>
+  );
+};
+
+export default Header;
